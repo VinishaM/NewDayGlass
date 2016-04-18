41e07b8a9cc733556e979feba98e9b6c80ebaf02
VinishaM <vm297@cornell.edu>
2016-04-18 19:23:22 -0400

    slight changes
    removed some files, renamed folder

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>New Day Glass</title>
+        <meta charset= "utf-8"/>
+        <link rel="stylesheet" href="css/styles.css" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Raleway%7COswald%7CMouse+Memoirs' rel='stylesheet' type='text/css'>
+    </head>
+    <body class="intro">
+        <div id="content">
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script>
+            //set current page as active page
+            $(document).ready(function() {
+                $('#' + $('body').attr('class')).addClass("active");
+            });
+        </script>
+    </body>
+</html>
